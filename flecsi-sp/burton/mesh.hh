472d316eae22e5d4d4a7c0e4b0eb09337c5934c2
Ben Bergen <bergen@lanl.gov>
2021-03-11 15:48:14 -0500

    Initial Check-In 2.0 Refactor

---
+++ flecsi-sp/burton/mesh.hh
@@ -0,0 +1,9 @@
+/*
+  Copyright (c) 2021, Triad National Security, LLC
+  All rights reserved
+ */
+
+#prgma once
+
+namespace fsp {
+} // namespace fsp
---
+++ flecsi-sp/library.cmake
@@ -0,0 +1,7 @@
+#------------------------------------------------------------------------------#
+# Copyright (c) 2021, Tiad National Security, LLC
+# All rights reserved
+#------------------------------------------------------------------------------#
+
+set(flecsi_sp_HEADERS)
+set(flecsi_sp_SOURCES)
