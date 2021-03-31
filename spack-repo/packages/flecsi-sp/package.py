9d164d3b78fb46442ffe27d6745b264d2d8d4f95
Ben Bergen <bergen@lanl.gov>
2021-03-31 10:27:22 -0600

    Updating build

--- spack-repo/packages/flecsi-sp/package.py
+++ spack-repo/packages/flecsi-sp/package.py
@@ -8,19 +8,31 @@ from spack import *
 import os
 
 
-class Flecsi(CMakePackage):
+class FlecsiSp(CMakePackage):
     '''FleCSI-SP is a repository for FleCSI specialization examples.
     '''
     homepage = 'http://flecsi.org/'
     git      = 'https://github.com/flecsi/flecsi-sp.git'
 
-    version('2.0', branch='2', submodules=False, preferred=True)
+    version('2.0', branch='devel', submodules=False, preferred=True)
+
+    #--------------------------------------------------------------------------#
+    # Variants
+    #--------------------------------------------------------------------------#
+
+    variant('flog', default=False,
+            description='Enable FLOG Logging Utility')    
+
+    variant('unit', default=False,
+            description='Enable Unit Tests (Requires +flog)')
 
     #--------------------------------------------------------------------------#
     # Dependencies
     #--------------------------------------------------------------------------#
 
     depends_on('flecsi@2.0:')
+    depends_on('flecsi+flog',when='+flog')
+    depends_on('flecsi+flog',when='+unit')
 
     #--------------------------------------------------------------------------#
     # CMake Configuration
@@ -29,5 +41,8 @@ class Flecsi(CMakePackage):
     def cmake_args(self):
         spec = self.spec
         options = []
+
+        if '~unit' in spec:
+            options.append('-DENABLE_UNIT_TESTS=OFF')        
 
         return options
